a7916cc40d191ed2315d655ed5684a8a64879395
SehajparkashSingh-BY <Sehajparkashsingh.Pannu@blueyonder.com>
2024-04-16 12:52:21 +0530

    DI in Java Configuration

--- src/main/java/guru/springframework/sfgdi/services/I18nEnglishGreetingService.java
+++ src/main/java/guru/springframework/sfgdi/services/I18nEnglishGreetingService.java
@@ -1,5 +1,7 @@
 package guru.springframework.sfgdi.services;
 
+import guru.springframework.sfgdi.repositories.EnglishGreetingRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 
@@ -9,8 +11,16 @@ import org.springframework.stereotype.Service;
 //@Profile("EN")
 //@Service("i18nService")
 public class I18nEnglishGreetingService implements GreetingService {
+
+    private final EnglishGreetingRepository englishGreetingRepository;
+
+    public I18nEnglishGreetingService(EnglishGreetingRepository englishGreetingRepository){
+        this.englishGreetingRepository = englishGreetingRepository;
+    }
     @Override
     public String sayGreeting() {
-        return "Hello World - EN";
+
+        return englishGreetingRepository.getGreeting();
+//        return "Hello World - EN";
     }
 }
